ae724db755b4f5287f52449aa232869148b63e69
hukuokakenhikichiyuki <em197872@icloud.com>
2020-09-18 17:50:26 +0900

    usersコントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
 
   def index
+    @users =User.all
   end
 
-  def edit
+  def new
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
