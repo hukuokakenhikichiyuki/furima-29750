9ff1d49b715773b4fdc21ef8bbc5629b18ae2f12
hukuokakenhikichiyuki <em197872@icloud.com>
2020-09-22 17:09:36 +0900

    アソシエーションとばりデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :user
+    has_one_attached :image
     belongs_to_active_hash :category
     belongs_to_active_hash :product_condition
     belongs_to_active_hash :shipping_fee_burden
     belongs_to_active_hash :shipping_area
     belongs_to_active_hash :estimated_shipping_date
 
-    validates_presence_of :category, :product_condition, :shipping_fee_burden, :shipping_area, :estimated_shipping_date
+    validates_presence_of :image, :name, :price, :description, :category_id, :product_condition_id, :shipping_fee_burden_id, :shipping_area_id, :estimated_shipping_date_id
 
-    validates :category, :product_condition, :shipping_fee_burden, :shipping_area, :estimated_shipping_date, numericality: { other_than: 1 }
+    validates :category_id, :product_condition_id, :shipping_fee_burden_id, :shipping_area_id, :estimated_shipping_date_id, numericality: { other_than: 1 }
 end
