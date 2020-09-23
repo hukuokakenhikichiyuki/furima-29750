36484ad5d7e2d31722dbe054797264ec2dc427ff
hukuokakenhikichiyuki <em197872@icloud.com>
2020-09-23 19:11:00 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,25 @@
 class ItemsController < ApplicationController
-    def index
-      @items = Item.order("created_at DESC")
-    end
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
 
-    def new
-      @item = Item.new
-    end
+  def new
+    redirect_to user_session_path unless user_signed_in?
+    @item = Item.new
+  end
 
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def item_params
-      params.require(:item).permit(:image, :name, :price, :description, :category_id, :product_condition_id, :shipping_fee_burden_id, :shipping_area_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
-    end
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :description, :category_id, :product_condition_id, :shipping_fee_burden_id, :shipping_area_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
+  end
 end
