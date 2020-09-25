1e3f32405dac2e8c67d052fbd6eeb56478924dd7
hukuokakenhikichiyuki <em197872@icloud.com>
2020-09-25 14:24:56 +0900

    リダイレクト先の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
       if @item.destroy
         redirect_to root_path
       else
-        redirect_to :edit
+        redirect_to :show
       end
     end
   end
