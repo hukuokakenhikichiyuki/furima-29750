db609d407f67af742fa13fbeb292e8829c573287
hukuokakenhikichiyuki <em197872@icloud.com>
2020-10-01 16:15:40 +0900

    商品購入機能のテストコード

---
+++ spec/factories/user_purchases.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user_purchase do
+    # association :user
+    # association :item
+    token              { 'あ' }
+    postcode           { '123-1234' }
+    shipping_area_id   {2}
+    cities             { '横浜市緑区' }
+    address            { '青山1-1-1' }
+    building_name      { '柳ビル103' }
+    phone_number       { '09012345678' }
+  end
+end
