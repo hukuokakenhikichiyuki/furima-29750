329e7e15ed12a9c091282ebd2237d5805809ce80
hukuokakenhikichiyuki <em197872@icloud.com>
2020-10-01 16:17:57 +0900

    クレジット機能の追加

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,23 +8,23 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("user_purchase[number]"),
+      cvc: formData.get("user_purchase[cvc]"),
+      exp_month: formData.get("user_purchase[exp_month]"),
+      exp_year: `20${formData.get("user_purchase[exp_year]")}`,
     };
-     console.log(card)
-    PayJp.createToken(card, (status, response) => {
+    console.log(card)
+    Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token =response.id;
-        const renserDom = document.getElementById("change-form");
+        const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("number").removeAttribute("name");
-      document.getElementById("cvc").removeAttribute("name");
-      document.getElementById("exp_month").removeAttribute("name");
-      document.getElementById("exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
       document.getElementById("charge-form").reset();
