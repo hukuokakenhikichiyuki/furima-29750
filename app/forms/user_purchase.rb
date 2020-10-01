b096900f6bb5330cb4e9893c45be7284c2b5d9c6
hukuokakenhikichiyuki <em197872@icloud.com>
2020-10-01 16:17:21 +0900

    フォームオブジェクト

--- app/forms/user_purchase.rb
+++ app/forms/user_purchase.rb
@@ -1,18 +1,23 @@
-class UserDonation
+class UserPurchase
 
   include ActiveModel::Model
-  attr_accessor :token, :postcode, :shipping_area_id, :cities,  :address, :building_name, :phone_number, :price
+  attr_accessor :token, :postcode, :shipping_area_id, :cities, :address, :building_name, :phone_number, :user_id, :item_id
 
-  validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  with_options presence: true do
+    validates :token, :postcode, :cities, :address, :phone_number, presence: true
 
-  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Postcode Input correctly"}
+    validates :postcode, length: { minimum: 7 }, allow_nil: true
 
-  validates :phone_number, format: {with: /\A[0-9]{3}[0-9]{4}[0-9]{4}\z/, message: "is not invalid. Include hyphen(-)"}
+    validates :shipping_area_id, numericality: { other_than: 1, message: "Shipping area must be other than 1" }
+
+    validates :phone_number, length: { maximum: 11, message: "電話番号は11桁以内で登録してください" }, allow_nil: true
+  end
 
   def save
-    user = User.create(token: token)
-    item = Item.create(image: image, name: name, price: price, description: description, category_id: category_id, product_condition_id: product_condition_id, shipping_fee_burden_id: shipping_fee_burden_id, shipping_area_id: shipping_area_id, estimated_shipping_date_id: estimated_shipping_date_id, user_id: user_id)
-    Order.create(postcode: postcode, shipping_area_id: shipping_area_id, cities: cities, address: address, building_name: building_name, phone_number: phone_number, user_id: user_id)
-    Purchase.create(item_id: item_id, user_id: user_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Order.create(postcode: postcode, shipping_area_id: shipping_area_id, cities: cities, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
+
+#/\A[0-9]{3}[0-9]{4}[0-9]{4}\z/
